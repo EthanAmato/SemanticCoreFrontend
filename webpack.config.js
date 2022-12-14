aacb42f8de6f189259f6531071ff100474adb016
EthanAmato <ethannamato@gmail.com>
2022-12-14 10:23:35 -0500

    added to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,6 @@ module.exports = {
         {
           test: /\.html$/i,
           loader: "html-loader",
-          options: {
-            // Disables attributes processing
-            sources: false,
-          },
         },
       ],
     },
